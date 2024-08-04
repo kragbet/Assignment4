98a27da176f24a2c88d0f4da046c86b548e9d21a
kragbet <35230590+kragbet@users.noreply.github.com>
2024-08-04 01:10:13 -0400

    attempt 1

--- App.java
+++ App.java
@@ -1,21 +1,53 @@
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
 import java.util.Scanner;
 
 public class App {
 
-    public static int countCharacters(String text) 
-}
-
-    public static int countWords(String text) {
-
+    public static int countCharacters(String text)
+    {
+        return text.length();
     }
 
-    public static int countLines(String text) {
+    public static int countWords(String text) 
+    {
+        String[] words = text.split("\\w+");
+        return words.length; 
+    }
+
+    public static int countLines(String text) 
+    {
         String[] lines = text.split("\r\n|\r|\n");
         return lines.length;
     } 
     public static void main(String[] args) {
+
+        //ask for file name
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the file name: ");
+        String fileName = scanner.nextLine();
+
+        // File theFile = new File("\""+fileName+"\"");
+        File theFile = new File(fileName);
+        
+
+        try (Scanner fileScanner = new Scanner(theFile))
+        {
+            String content = theFile.toString();
+
+            int numCharacters = countCharacters(theFile.toString());
+            int numWords = countWords(content);
+            int numLines = countLines(content);
+            
+            System.out.println("Number of characters: " + numCharacters);
+            System.out.println("Number of words: " + numWords);
+            System.out.println("Number of lines: " + numLines);
+            
+        } catch (Exception e) {
+            System.out.println("Error: The file '" + fileName + "' was not found.");
+        }
+        scanner.close();
+
+       
         
     }
 }
---
+++ test2.java
@@ -0,0 +1,8 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class test2 {
+public static void main(String[] args) {
+    
+}
+}
