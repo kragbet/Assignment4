123dba283759d53f4de51bf1d3e3f8b5a9ecf3ce
kragbet <35230590+kragbet@users.noreply.github.com>
2024-08-03 23:56:11 -0400

    initialized application

---
+++ App.java
@@ -0,0 +1,21 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+
+    public static int countCharacters(String text) 
+}
+
+    public static int countWords(String text) {
+
+    }
+
+    public static int countLines(String text) {
+        String[] lines = text.split("\r\n|\r|\n");
+        return lines.length;
+    } 
+    public static void main(String[] args) {
+        
+    }
+}
